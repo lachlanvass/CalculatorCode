06415aee41f6171858cbae274f8658483bc48692
Lachlan Vass <lachlan.vass@hotmail.com>
2018-12-24 08:28:02 +1000

    Added age calculator. Need to remove requirement for time. Not relevant to calculation.

---
+++ TimeCalculator/Controllers/AgeCalculatorController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CalculatorCode.Models;
+
+namespace CalculatorCode.Controllers
+{
+    public class AgeCalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            SetViewBagValues();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(AgeCalculator model)
+        {
+            SetViewBagValues();
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }            
+
+            model.Result = model.CalculateAge();
+            return View(model);
+        }
+
+
+        public void SetViewBagValues()
+        {
+            ViewBag.Code = AlgorithmCode;
+            ViewBag.Instructions = "Enter your birthdate. See how many days old you are";
+
+        }
+
+        private String AlgorithmCode =
+@"public TimeSpan CalculateAge(DateTime InputOne)
+{
+    return DateTime.Now - InputOne;
+}";
+    }
+
+}
---
+++ TimeCalculator/Models/AgeCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorCode.Models
+{
+    public class AgeCalculator : Calculator
+    {
+        new public DateTime InputOne { get; set; }
+        new public TimeSpan Result;
+
+        public TimeSpan CalculateAge()
+        {
+            return DateTime.Now - InputOne;
+        }
+    }
+}
