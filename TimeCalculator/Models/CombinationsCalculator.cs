7953285afb07b5bde7959d3f00f28811baa6545a
Lachlan Vass <lachlan.vass@hotmail.com>
2019-01-08 07:43:08 +1000

    Added base files for Combinations and Permuatations Calculators.

---
+++ TimeCalculator/Models/CombinationsCalculator.cs
@@ -0,0 +1,40 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CalculatorCode.Models
+{
+    public class CombinationsCalculator : Calculator
+    {
+        [RegularExpression(@"^$", ErrorMessage = "Please enter a valid number")]
+        new public int InputOne { get; set; }
+
+        [RegularExpression(@"^$", ErrorMessage = "Please enter a valid number")]
+        new public int InputTwo { get; set; }
+        new public int Result { get; set; }
+
+        public String SelectedOperation { get; set; }
+ 
+        public int CalculateAdditionResult()
+        {
+            return InputOne + InputTwo;
+        }
+
+        public int CalculateSubtractionResult()
+        {
+            return InputOne - InputTwo;
+        }
+
+        public int CalculateMultiplyResult()
+        {
+            return InputOne * InputTwo;
+        }
+
+        public int CalculateDivideResult()
+        {
+            return InputOne / InputTwo;
+        }
+
+    }
+
+}
